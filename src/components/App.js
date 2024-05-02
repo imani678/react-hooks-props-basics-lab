3c62cddfb2bc9703c668e4ed2f703a2118e3566b
imani <18imani.vata@gmail.com>
2024-05-02 11:28:50 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -10,11 +11,15 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+    <NavBar />
+      
+      <Home name="Liza" city="New York" color="firebrick" />
+      
+      <About bio="I made this!"/>
+      
     </div>
   );
 }
+
 
 export default App;
